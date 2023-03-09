48ceaa69b0cbc0afbf9bca78e20d73e6207f820b
ChloeGlbrt <chloegalbert@hotmail.fr>
2023-03-09 09:54:39 +0100

    first commit

---
+++ src/main/java/com/inti/GestionContentieuxApplication.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionContentieuxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionContentieuxApplication.class, args);
+	}
+
+}
